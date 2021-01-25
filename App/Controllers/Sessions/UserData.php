610ccebd4b0f85f3d42ca544b5816ecd1f939454
Slava Kuleshov <slava3626@gmail.com>
2021-01-25 20:29:26 +0300

    rdy for sure?

--- App/Controllers/Sessions/UserData.php
+++ App/Controllers/Sessions/UserData.php
@@ -30,13 +30,9 @@ class UserData
     public function insertUserData(?string $login = null, ?string $pass = null, ?string $name = null, ?string $surname = null, ?int $phone = null, ?string $email = null)
     {
         $query = "INSERT INTO users (`login`, `password`, `name`, `surname`, `phone_number`, `email`) VALUES (?, ?, ?, ?, ?, ?)";
-
         $statement = $this->connection->prepare($query);
-
         $statement->setFetchMode(PDO::FETCH_ASSOC);
-        
         $result = $statement->execute([$login, $pass, $name, $surname, $phone, $email]);
-
         $this->lastInsertId = $this->connection->lastInsertId();
 
         return $result;
